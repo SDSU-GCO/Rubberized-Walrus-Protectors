1f1217a2c6559c65bb556a27d71b0a25e460980f
Swiftfire7 <34386895+Swiftfire7@users.noreply.github.com>
2019-06-01 16:28:13 -0500

    finished enemy 3 (and 4-6 by extension)

--- RWP/RWP/Assets/Scripts/Enemy_Sideways.cs
+++ RWP/RWP/Assets/Scripts/Enemy_Sideways.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_Sideways : MonoBehaviour
 {
+    public List<Transform> path;
     public float moveDelay = 3f;
     new Rigidbody2D rigidbody2D;
     private Vector2 velocity;
@@ -11,41 +13,70 @@ public class Enemy_Sideways : MonoBehaviour
     public float timePassed;
     bool switchDirection=false;
     public float idle=6f;
+    public AnimationCurve animationCurve = new AnimationCurve();
+    float pathProgress;
+    Transform nextTarget;
+    int pathIndex = 0;
+    int nextPathIndex = 1;
 
     private void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
+    private void OnEnable()
+    {
+        pathIndex = 0;
+        nextPathIndex = 1;
+        Debug.Assert(path.Count >= 2, "Error: path must contain at least two points!");
+    }
     private void Update()
     {
         //move
-        if (switchDirection == true && idle >= (moveDelay*2))
+        pathProgress += Time.deltaTime;
+        while (pathProgress >= 1)
         {
-            moveVelocity = -moveVelocity;
-            idle = 0;
+            pathProgress -= 1;
+            autoIncrement(ref pathIndex);
+            autoIncrement(ref nextPathIndex);
         }
-        else
-        {
-            rigidbody2D.velocity += Vector2.left*0;
-        }
+        Vector2 temp = Vector2.Lerp(path[pathIndex].position, path[nextPathIndex].position, animationCurve.Evaluate(pathProgress));
 
-        if (timePassed >= moveDelay && rigidbody2D.velocity.x <= 0.0000001)
-        {
-            rigidbody2D.velocity += Vector2.left* moveVelocity;
-            timePassed = 0;
-            
-            switchDirection = true;
-            
-        }
-        else
-        {
-            switchDirection = false;
-        }
+        Debug.Log(path[pathIndex].position);
+        rigidbody2D.MovePosition(temp);
 
-        timePassed += Time.deltaTime;
-        idle += Time.deltaTime;
+
+        //if (switchDirection == true && idle >= (moveDelay*2))
+        //{
+        //    moveVelocity = -moveVelocity;
+        //    idle = 0;
+        //}
+        //else
+        //{
+        //    rigidbody2D.velocity += Vector2.left*0;
+        //}
+
+        //if (timePassed >= moveDelay && rigidbody2D.velocity.x <= 0.0000001)
+        //{
+        //    rigidbody2D.velocity += Vector2.left* moveVelocity;
+        //    timePassed = 0;
+            
+        //    switchDirection = true;
+            
+        //}
+        //else
+        //{
+        //    switchDirection = false;
+        //}
+
+        //timePassed += Time.deltaTime;
+        //idle += Time.deltaTime;
         
     }
-    
-        
+
+    private void autoIncrement(ref int pathIndex)
+    {
+        pathIndex++;
+        if (pathIndex >= path.Count)
+            pathIndex = 0;
+    }
 }
