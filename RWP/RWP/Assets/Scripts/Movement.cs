3d528421de9bd29b76bb008b5629b698578c962b
Swiftfire7 <34386895+Swiftfire7@users.noreply.github.com>
2019-05-31 22:58:20 -0500

    made test zone for movement
    added sideways movement, jumping in progress.

---
+++ RWP/RWP/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    new Rigidbody2D rigidbody2D;
+    public float acceleration=0;
+    public float jumpForce=0;
+
+    private Vector2 totalForce=Vector2.zero;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.AddForce(totalForce);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        totalForce.x = Input.GetAxis("Horizontal")* acceleration;
+        if (Input.GetAxis("Vertical") > 0)
+            totalForce.y = jumpForce;
+        else
+            totalForce.y = 0;
+    }
+}
---
+++ RWP/RWP/Assets/Scripts/Movement_depricated.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_depricated : MonoBehaviour
+{
+    //speed variables
+    public float moveSpeed = 1;
+    public float jumpForce = 1;
+    public float maxSpeed = 3;
+    public float drag;
+    public float gravity = 1;
+
+    //reference variables
+    private Vector2 playerInput;
+    private Rigidbody2D thisRB;
+    private Vector2 acceleration=Vector2.zero;
+
+    private Vector2 velocity = Vector2.zero;
+
+
+    private void Awake()
+    {
+        if (thisRB == null)
+        {
+            thisRB = GetComponent<Rigidbody2D>();
+        }
+    }
+
+
+    void Update()
+    {
+
+        //jump
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            acceleration.y +=  jumpForce;
+        }
+
+        acceleration.y -= Time.deltaTime*gravity;
+
+        //movement 
+        acceleration.x = Input.GetAxis("Horizontal");
+
+        Debug.Log(velocity);
+
+        velocity = velocity + (acceleration * Time.deltaTime);
+        if(velocity.x>0)
+        {
+            velocity.x -= ((velocity.x + 1) * (drag * Time.deltaTime));
+            velocity.x = Mathf.Min(0, velocity.x);
+        }
+        else
+        {
+            velocity.x -= ((velocity.x - 1) * (drag * Time.deltaTime));
+            velocity.x = Mathf.Max(0, velocity.x);
+        }
+
+        if (velocity.y > 0)
+        {
+            velocity.y -= ((velocity.y + 1) * (drag * Time.deltaTime));
+            velocity.y = Mathf.Min(0, velocity.y);
+        }
+        else
+        {
+            velocity.y -= ((velocity.y - 1) * (drag * Time.deltaTime));
+            velocity.y = Mathf.Max(0, velocity.y);
+        }
+
+
+            Debug.Log(velocity);
+        //speed cap
+        if (velocity.magnitude > maxSpeed)
+        {
+            float factor = maxSpeed / velocity.magnitude;
+            Debug.Log(factor);
+            velocity.x *= factor;
+            velocity.y *= factor;
+        }
+        Debug.Log(velocity);
+
+        //normalize velocity
+        thisRB.MovePosition((velocity*Time.deltaTime)+thisRB.position);
+       
+        
+
+    }
+}
