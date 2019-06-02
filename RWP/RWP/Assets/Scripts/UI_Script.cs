798873ea41b44fdf99122746834a8a164a8b92b8
Swiftfire7 <34386895+Swiftfire7@users.noreply.github.com>
2019-06-02 17:51:34 -0500

    UI background and player attack

--- RWP/RWP/Assets/Scripts/UI_Script.cs
+++ RWP/RWP/Assets/Scripts/UI_Script.cs
@@ -7,12 +7,52 @@ using UnityEngine.SceneManagement;
 public class UI_Script : MonoBehaviour
 {
     public TextMeshProUGUI healthCounter;
+    public TextMeshProUGUI trees;
+    public TextMeshProUGUI enemies;
+    public TreeListSO treeCounter;
+    public EnemyListSO enemyCounter;
     public Entity_Logic entityLogic;
     public Canvas PauseMenu;
     public GameObject Druid;
     float health;
     public Canvas gameOverScreen;
     IEnumerator coroutine;
+    int MaxTreeCount = 0;
+    int MaxEnemyCount = 0;
+    Color tint = new Color(1.0f, 0, 1.0f, 1.0f);
+
+    private void Awake()
+    {
+        treeCounter.update.AddListener(updateTrees);
+        enemyCounter.update.AddListener(updateEnemies);
+    }
+
+    private void OnDestroy()
+    {
+        treeCounter.update.RemoveListener(updateTrees);
+        enemyCounter.update.RemoveListener(updateEnemies);
+    }
+
+    public void updateEnemies()
+    {
+        if (enemyCounter.enemies.Count > MaxEnemyCount)
+        {
+            MaxEnemyCount = enemyCounter.enemies.Count;
+        }
+        enemies.SetText("Trees Left: " + enemyCounter.enemies.Count);
+        Color temp = Color.Lerp(tint, Color.white, enemyCounter.enemies.Count / (MaxTreeCount + MaxEnemyCount));
+    }
+
+    public void updateTrees()
+    {
+        if(treeCounter.trees.Count>MaxTreeCount)
+        {
+            MaxTreeCount = treeCounter.trees.Count;
+        }
+        trees.SetText("Trees Left: " + treeCounter.trees.Count);
+        Color temp = Color.Lerp(tint, Color.white, treeCounter.trees.Count / (MaxTreeCount+MaxEnemyCount));
+    }
+    
 
     private void Update()
     {
@@ -37,6 +77,8 @@ public class UI_Script : MonoBehaviour
 
     private void OnEnable()
     {
+        updateEnemies();
+        updateTrees();
         entityLogic.damaged.AddListener(updateHealth);
     }
 
