c5d005d20c2881c333f3e407d3c1d951a53e192a
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-06-25 17:38:48 -0600

    Added custom hashtable and hashset
    ...I don't remember why I wrote a custom implementation of these...

---
+++ RWP/RWP/Assets/Scripts/HashSet.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashSet<Value>
+{
+    private int maxCount;
+    private readonly float ratioToFill;
+    private int currentCount = 0;
+    private List<ValueSlot> valueSlots = new List<ValueSlot>();
+
+    private struct ValueSlot
+    {
+        public bool hasValue;
+        public List<Value> values;
+    }
+
+    public struct ResultantValue
+    {
+        public bool hasValue;
+        private Value value;
+        public Value Value
+        {
+            get
+            {
+                if (hasValue)
+                {
+                    return value;
+                }
+                else
+                {
+                    throw new NullReferenceException("Null reference Ecxeption trying to use a value from hash table!");
+                }
+            }
+            set
+            {
+                hasValue = true;
+                this.value = value;
+            }
+        }
+    }
+
+    public void AddValue(Value value)
+    {
+        currentCount++;
+        if (currentCount < (maxCount * ratioToFill))
+        {
+            AddValue(valueSlots, value);
+        }
+        else
+        {
+            int newSize = maxCount * 2;
+            List<ValueSlot> newList = GetNewListOfSlots(newSize);
+            foreach (ValueSlot valueSlot in valueSlots)
+            {
+                if (valueSlot.hasValue == true)
+                {
+                    foreach (Value oldValue in valueSlot.values)
+                    {
+                        AddValue(newList, oldValue);
+                    }
+                }
+            }
+            maxCount = newSize;
+            valueSlots = newList;
+        }
+    }
+
+    private void AddValue(List<ValueSlot> valueSlots, Value value)
+    {
+        int UnresolvedHash = value.GetHashCode();
+
+        int ResolvedHash = UnresolvedHash.GetHashCode() % valueSlots.Count;
+
+        if (valueSlots[ResolvedHash].hasValue != true)
+        {
+            ValueSlot tmp = new ValueSlot
+            {
+                hasValue = true,
+                values = new List<Value>()
+            };
+            tmp.values.Add(value);
+
+            valueSlots[ResolvedHash] = tmp;
+        }
+        else
+        {
+            valueSlots[ResolvedHash].values.Add(value);
+        }
+    }
+
+    public ResultantValue TryGetValue(Value value)
+    {
+        ResultantValue tmp = default;
+        (bool result, Value myVal) = TryGetValue(valueSlots, value);
+
+        if (result)
+        {
+            tmp.Value = myVal;
+        }
+        else
+        {
+            tmp.hasValue = false;
+        }
+
+        return tmp;
+    }
+    public bool ContainsValue(Value value) => ContainsValue(valueSlots, value);
+    public bool RemoveValue(Value value) => RemoveValue(valueSlots, value);
+
+    private (bool, Value) TryGetValue(List<ValueSlot> valueSlots, Value value)
+    {
+        Value rtnVal = default;
+        bool successful = false;
+        int UnresolvedHash = value.GetHashCode();
+
+        int ResolvedHash = UnresolvedHash.GetHashCode() % valueSlots.Count;
+
+        if (valueSlots[ResolvedHash].hasValue == true)
+        {
+            foreach (Value tmpVal in valueSlots[ResolvedHash].values)
+            {
+                if (tmpVal.Equals(value))
+                {
+                    rtnVal = tmpVal;
+                    successful = true;
+                }
+            }
+        }
+
+        return (successful, rtnVal);
+    }
+
+    private bool ContainsValue(List<ValueSlot> valueSlots, Value value)
+    {
+        bool rtnVal = false;
+        int UnresolvedHash = value.GetHashCode();
+
+        int ResolvedHash = UnresolvedHash.GetHashCode() % valueSlots.Count;
+
+        if (valueSlots[ResolvedHash].hasValue == true)
+        {
+            rtnVal = valueSlots[ResolvedHash].values.Contains(value);
+        }
+
+        return rtnVal;
+    }
+
+    private bool RemoveValue(List<ValueSlot> valueSlots, Value value)
+    {
+        bool rtnVal = false;
+        int UnresolvedHash = value.GetHashCode();
+
+        int ResolvedHash = UnresolvedHash.GetHashCode() % valueSlots.Count;
+
+        if (valueSlots[ResolvedHash].hasValue == true)
+        {
+            rtnVal = valueSlots[ResolvedHash].values.Remove(value);
+        }
+        return rtnVal;
+    }
+
+    public HashSet(float ratioToFill = 0.5f, int initialSize = 64)
+    {
+        Debug.Assert(initialSize > 0, "HashTables must have an initial size >= 1");
+        maxCount = initialSize;
+        Debug.Assert(ratioToFill > 0f && ratioToFill < 1f, "HashTables must have a ratio to keep full between 0f & 1f");
+        this.ratioToFill = ratioToFill;
+
+        valueSlots = GetNewListOfSlots(maxCount);
+    }
+
+    private List<ValueSlot> GetNewListOfSlots(int sizeOfList)
+    {
+        List<ValueSlot> valueSlots = new List<ValueSlot>();
+        for (int i = 0; i < sizeOfList; i++)
+        {
+            ValueSlot tmp = new ValueSlot
+            {
+                hasValue = false,
+                values = null,
+            };
+
+            valueSlots.Add(tmp);
+        }
+
+        return valueSlots;
+    }
+}
---
+++ RWP/RWP/Assets/Scripts/HashTable.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HashTable<Key, Value>
+{
+    private int maxCount;
+    private readonly float ratioToFill;
+    private int currentCount = 0;
+    private List<ValueSlot> valueSlots = new List<ValueSlot>();
+
+    private struct ValueSlot
+    {
+        public bool hasValue;
+        public List<KeyValuePair<Key, Value>> values;
+    }
+
+    public struct ResultantValue
+    {
+        public bool hasValue;
+        private Value value;
+        public Value Value
+        {
+            get
+            {
+                if (hasValue)
+                {
+                    return value;
+                }
+                else
+                {
+                    throw new NullReferenceException("Null reference Ecxeption trying to use a value from hash table!");
+                }
+            }
+            set
+            {
+                hasValue = true;
+                this.value = value;
+            }
+        }
+    }
+
+    public HashTable(float ratioToFill = 0.5f, int initialSize = 64)
+    {
+        Debug.Assert(initialSize > 0, "HashTables must have an initial size >= 1");
+        maxCount = initialSize;
+        Debug.Assert(ratioToFill > 0f && ratioToFill < 1f, "HashTables must have a ratio to keep full between 0f & 1f");
+        this.ratioToFill = ratioToFill;
+
+        valueSlots = GetNewListOfSlots(maxCount);
+    }
+
+    private List<ValueSlot> GetNewListOfSlots(int sizeOfList)
+    {
+        List<ValueSlot> valueSlots = new List<ValueSlot>();
+        for (int i = 0; i < sizeOfList; i++)
+        {
+            ValueSlot tmp = new ValueSlot
+            {
+                hasValue = false,
+                values = null,
+            };
+
+            valueSlots.Add(tmp);
+        }
+
+        return valueSlots;
+    }
+
+    public void AddValue(Key key, Value value)
+    {
+        currentCount++;
+        if (currentCount < (maxCount * ratioToFill))
+        {
+            AddValue(valueSlots, key, value);
+        }
+        else
+        {
+            int newSize = maxCount * 2;
+            List<ValueSlot> newList = GetNewListOfSlots(newSize);
+            foreach (ValueSlot valueSlot in valueSlots)
+            {
+                if (valueSlot.hasValue == true)
+                {
+                    foreach (KeyValuePair<Key, Value> kvp in valueSlot.values)
+                    {
+                        AddValue(newList, kvp.Key, kvp.Value);
+                    }
+                }
+            }
+            maxCount = newSize;
+            valueSlots = newList;
+        }
+    }
+
+    private bool AddValue(List<ValueSlot> valueSlots, Key key, Value value)
+    {
+        bool rtnVal = true;
+
+        int UnresolvedHash = key.GetHashCode();
+
+        int ResolvedHash = UnresolvedHash.GetHashCode() % valueSlots.Count;
+
+        if (valueSlots[ResolvedHash].hasValue != true)
+        {
+            ValueSlot tmp = new ValueSlot
+            {
+                hasValue = true,
+                values = new List<KeyValuePair<Key, Value>>()
+            };
+            KeyValuePair<Key, Value> keyValuePair = new KeyValuePair<Key, Value>(key, value);
+
+            tmp.values.Add(keyValuePair);
+
+            valueSlots[ResolvedHash] = tmp;
+        }
+        else
+        {
+            KeyValuePair<Key, Value> keyValuePair = new KeyValuePair<Key, Value>(key, value);
+
+            rtnVal = false;
+            foreach (KeyValuePair<Key, Value> kvp in valueSlots[ResolvedHash].values.TakeWhile(x => rtnVal == false))
+            {
+                if (kvp.Key.Equals(keyValuePair.Key))
+                {
+                    rtnVal = true;
+                }
+            }
+            if (rtnVal == true)
+            {
+                valueSlots[ResolvedHash].values.Add(keyValuePair);
+            }
+        }
+        return rtnVal;
+    }
+
+    public ResultantValue TryGetValue(Key key)
+    {
+        ResultantValue tmp = default;
+        (bool result, Value myVal) = TryGetValue(valueSlots, key);
+
+        if (result)
+        {
+            tmp.Value = myVal;
+        }
+        else
+        {
+            tmp.hasValue = false;
+        }
+
+        return tmp;
+    }
+
+    public bool ContainsValue(Key key) => ContainsValue(valueSlots, key);
+    public bool RemoveValue(Key key) => RemoveValue(valueSlots, key);
+
+    private (bool, Value) TryGetValue(List<ValueSlot> valueSlots, Key key)
+    {
+        Value rtnVal = default;
+        bool successful = false;
+        int UnresolvedHash = key.GetHashCode();
+
+        int ResolvedHash = UnresolvedHash.GetHashCode() % valueSlots.Count;
+
+        if (valueSlots[ResolvedHash].hasValue == true)
+        {
+            foreach (KeyValuePair<Key, Value> kvp in valueSlots[ResolvedHash].values.TakeWhile(x => successful == false))
+            {
+                if (kvp.Key.Equals(key))
+                {
+                    rtnVal = kvp.Value;
+                    successful = true;
+                }
+            }
+        }
+
+        return (successful, rtnVal);
+    }
+
+    private bool ContainsValue(List<ValueSlot> valueSlots, Key key)
+    {
+        bool rtnVal = false;
+        int UnresolvedHash = key.GetHashCode();
+
+        int ResolvedHash = UnresolvedHash.GetHashCode() % valueSlots.Count;
+
+        if (valueSlots[ResolvedHash].hasValue == true)
+        {
+            foreach (KeyValuePair<Key, Value> kvp in valueSlots[ResolvedHash].values.TakeWhile(x => rtnVal == false))
+            {
+                if (kvp.Key.Equals(key))
+                {
+                    rtnVal = true;
+                }
+            }
+        }
+
+        return rtnVal;
+    }
+
+    private bool RemoveValue(List<ValueSlot> valueSlots, Key key)
+    {
+        bool rtnVal = false;
+        int UnresolvedHash = key.GetHashCode();
+
+        int ResolvedHash = UnresolvedHash.GetHashCode() % valueSlots.Count;
+
+        if (valueSlots[ResolvedHash].hasValue == true)
+        {
+            KeyValuePair<Key, Value> kvpToRemove;
+            foreach (KeyValuePair<Key, Value> kvp in valueSlots[ResolvedHash].values.TakeWhile(x => rtnVal == false))
+            {
+                if (kvp.Key.Equals(key))
+                {
+                    rtnVal = true;
+                    kvpToRemove = kvp;
+                }
+            }
+
+            if (rtnVal == true)
+            {
+                rtnVal = valueSlots[ResolvedHash].values.Remove(kvpToRemove);
+            }
+        }
+        return rtnVal;
+    }
+}
