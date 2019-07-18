9bafba7a60dfc32a3ab0f44f684f95a1d58b2677
Swiftfire7 <34386895+Swiftfire7@users.noreply.github.com>
2019-07-18 09:29:11 -0500

    added title screen and win screen

---
+++ RWP/RWP/Assets/Scripts/Next_Slide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Next_Slide : MonoBehaviour
+{
+    public Canvas thisCanvas;
+    public Canvas nextCanvas;
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            thisCanvas.enabled = false;
+            //this is supposed to be empty for now, it's here for a reference to the opening cutscene/next part of the cutscene. Think Undertale's opening slideshow, that's how I'm picturing this.
+            nextCanvas.enabled = true;
+        }
+    }
+}
---
+++ RWP/RWP/Assets/Scripts/Win_Screen_Check.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win_Screen_Check : MonoBehaviour
+{
+    public Canvas winScreenCanvas;
+    public bool treesCured;
+    public bool enemiesCured;
+
+    private void Update()
+    {
+        //so right now this doesn't work, I'm too tired to think lol, it needs to check if the enemies and trees are cured then enable itself.
+        if (treesCured == true && enemiesCured == true)
+        {
+            winScreenCanvas.enabled = true;
+        }
+    }
+        
+}
